4b2c7d755d023f4d5ece94bb520031219349b769
brunanunes <brunanunes324@gmail.com>
2023-12-18 09:51:00 -0300

    second commit

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ document.addEventListener('DOMContentLoaded', () =>{
     let birdBottom = 100
     let gravity = 2
     let isGameOver = false
+    let gap = 430
 
 
     //function that make the game works
@@ -48,25 +49,36 @@ document.addEventListener('DOMContentLoaded', () =>{
         //creating a div
         const obstacle = document.createElement('div')
         //adding a class into a div
-        obstacle.classList.add('obstacle')
+        const topObstacle = document.createElement('div')
+        if(!isGameOver){
+             obstacle.classList.add('obstacle')
+             topObstacle.classList.add('topObstacle')}//if the game is not over the obstacle will show up
         //adding the div into the game-container
         gameDisplay.appendChild(obstacle)
+        gameDisplay.appendChild(topObstacle)
         obstacle.style.left = obstacleLeft + 'px'
+        topObstacle.style.left = obstacleLeft + 'px'
         obstacle.style.bottom = obstacleBottom + 'px'
+        topObstacle.style.bottom = obstacleBottom + gap + 'px'
 
         function moveObstacle(){
             obstacleLeft -= 2
             obstacle.style.left = obstacleLeft + 'px'
+            topObstacle.style.left = obstacleLeft + 'px'
 
             //when the obstacle hit "the wall" the timer will stop from executing
             if(obstacleLeft == -60){
                 clearInterval(timerId)
                 //then the obstacle will be removed
                 gameDisplay.removeChild(obstacle)
+                gameDisplay.removeChild(topObstacle)
+
             }
             if(
-                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 ||
-                birdBottom == 0
+                //the bird will stop moving when you hit a obstacle
+                obstacleLeft > 200 && obstacleLeft < 280 && birdLeft === 220 &&
+                (birdBottom < obstacleBottom + 153 || birdBottom > obstacleBottom + gap -200)||
+                birdBottom === 0
                 ){
                 gameOver()
             }
@@ -74,12 +86,13 @@ document.addEventListener('DOMContentLoaded', () =>{
         //invoking the function multiple times
         let timerId = setInterval(moveObstacle, 20)
         //it will generate another obstacle every 3 seconds
-        setTimeout(generateObstcle, 3000)
+        if(!isGameOver) setTimeout(generateObstcle, 3000)
     }
     generateObstcle()
 
     function gameOver(){
         clearInterval(gameTimerId)
+        console.log('game over')
         isGameOver = true
         document.removeEventListener('keyup', control)
     }
